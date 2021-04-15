138cab963bdee9456fd949924e35295ec9f99942
Arjunsing1997 <acchur1997@gmail.com>
2021-04-15 16:06:46 +0530

    UC3-Checking Options

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -16,6 +16,32 @@ namespace SnakeAndLadder
             Random random = new Random(); //Creating an object from Random class
             int diceValue = random.Next(1, 7); //Generating Random values from 1 to 6
             Console.WriteLine("Dice Value: " + diceValue);
+            CheckOption(diceValue);
+        }
+
+        public static void CheckOption(int diceValue)    //UC3
+        {
+
+            Random random = new Random();
+            int option = random.Next(1, 4);     //generating a random number from 1 to 3
+
+            switch (option) //Using switch case to choose ladder and snake in the game
+            {
+                case 1:
+                    Console.WriteLine("No Play!!!!");   //this case for NO PLAY
+                    Console.WriteLine("POSITION: " + position);
+                    break;
+                case 2:
+                    Console.WriteLine("Ladder..."); //this case is for LADDER
+                    position = position + diceValue;
+                    Console.WriteLine("POSITION: " + position);
+                    break;
+                case 3:
+                    Console.WriteLine("Snake..."); //this case is for SNAKE
+                    position = position - diceValue;
+                    Console.WriteLine("POSITION: " + position);
+                    break;
+            }
         }
     }
 }
