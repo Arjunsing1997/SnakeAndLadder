b295daa75fb6e577f1e3b677b51b84e9b2a713c7
Arjunsing1997 <acchur1997@gmail.com>
2021-04-15 16:00:55 +0530

    UC2-Dice_Roll

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -8,7 +8,14 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE AND LADDER");
+            DiceRoll();
         }
 
+        public static void DiceRoll()  //UC2
+        {
+            Random random = new Random(); //Creating an object from Random class
+            int diceValue = random.Next(1, 7); //Generating Random values from 1 to 6
+            Console.WriteLine("Dice Value: " + diceValue);
+        }
     }
 }
