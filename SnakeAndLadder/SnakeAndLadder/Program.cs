1529b1a1f4951e8051f1db68d77de1b042ecad22
Arjunsing1997 <acchur1997@gmail.com>
2021-04-15 17:10:33 +0530

    UC5-Exact 100th Position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -5,10 +5,15 @@ namespace SnakeAndLadder
     class Program
     {
         static int position = 0;
+        const int Max_Position = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE AND LADDER");
-            DiceRoll();
+            while(position < Max_Position)//to Execute until reach 100th Position
+            {
+                DiceRoll(); //Calling the Dice Roll method
+            }
+
         }
 
         public static void DiceRoll()  //UC2
@@ -34,19 +39,24 @@ namespace SnakeAndLadder
                 case 2:
                     Console.WriteLine("Ladder..."); //this case is for LADDER
                     position = position + diceValue;
+                    Position(diceValue);
                     Console.WriteLine("POSITION: " + position);
                     break;
                 case 3:
                     Console.WriteLine("Snake..."); //this case is for SNAKE
                     position = position - diceValue;
-                    Position();
+                    Position(diceValue);
                     Console.WriteLine("POSITION: " + position);
                     break;
             }
         }
-        public static void Position() //UC4
+        public static void Position(int  diceValue) //UC4
         {
-            if (position < 0)  //condition to check wheter position is lss than 0
+            if (position > 100)  //To check whether position Crossing the 100th line
+            {
+                position = position - diceValue; //if Position crosses the 100th posotion then we are making the position value as Previous 
+            }
+            else if (position < 0)  //condition to check wheter position is lss than 0
             {
                 position = 0; //if position value is negative then we are assigning 0 to 
             }
