7606904ca29d0758ab5d687e90f13cdf4a4d5879
Arjunsing1997 <acchur1997@gmail.com>
2021-04-15 15:55:50 +0530

    UC1-Position Zero With welcome Message

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static int position = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SNAKE AND LADDER");
+        }
+
+    }
+}
